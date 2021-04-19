a06630301f45c93cca9c2bce3a7b4700240b6f4f
Arsalan Khan <arsalankhan612621@gmail.com>
2021-04-19 14:08:19 -0700

    Practice Java  Script
    added CSS codes and added required field validation Java Script

--- Practice Project/script.js
+++ Practice Project/script.js
@@ -5,7 +5,25 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// Funcion to update class and message for errors
+function showError(input, message) {
+    // Get the parent element of the input field (.form-control)
+    const formControl = input.parentElement;
+    // Replace the class - add error
+    formControl.className = 'form-control error';
+    // Get the small element for the error message
+    const small = formControl.querySelector('small');
+    // Replace the text for small element using the input message
+    small.innerText = message;
+}
 
+// Function to update class for success
+function showSuccess(input) {
+   // Get the parent element of the input field (.form-control)
+   const formControl = input.parentElement;
+   // Replace the class - add success
+   formControl.className = 'form-control success'; 
+}
 
 // Event Listeners
 // Create event listener for submit button
@@ -15,6 +33,29 @@ form.addEventListener('submit', function(e) {
 
     // Check if username is empty
     if(username.value === '')  {
-        alert('Username is required');
+        showError(username, 'Username is required');
+    } else {
+        showSuccess(username);
+    }
+
+    // Check if email is empty
+    if(email.value === '')  {
+        showError(email, 'Email is required');
+    } else {
+        showSuccess(email);
+    }
+
+    // Check if password is empty
+    if(password.value === '')  {
+        showError(password, 'Password is required');
+    } else {
+        showSuccess(password);
+    }
+
+    // Check if password2 is empty
+    if(password2.value === '')  {
+        showError(password2, 'Confirm Password is required');
+    } else {
+        showSuccess(password2);
     }
 });
