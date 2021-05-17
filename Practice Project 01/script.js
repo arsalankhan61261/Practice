afa9780543a6c44430957646cb37d0522a1c3e3f
Arsalan Khan <arsalankhan612621@gmail.com>
2021-05-17 16:30:31 -0700

    Update script.js

--- Practice Project 01/script.js
+++ Practice Project 01/script.js
@@ -28,7 +28,11 @@ function showSuccess(input) {
 // Function to check if email is valid
 function isValidEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, `${getFieldId(input)} is not a valid Email`);
+    }
 }
 
 // Function to check if required fields have data
@@ -43,11 +47,21 @@ function checkRequired(inputArray) {
     });
 }
 
+// Function to check length of input field
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldId(input)} needs to be at least ${min} characters`)
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldId(input)} needs to be less than ${max} characters`)
+    } else {
+        showSuccess(input);
+    }
+}
+
 // Function to get the id of input field
 function getFieldId(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
-
 
 // Event Listeners
 // Create event listener for submit button
@@ -56,4 +70,7 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     checkRequired([username,email,password,password2]);
+    checkLength(username,3,10);
+    checkLength(password,6,30);
+    checkEmail(email);
 });
