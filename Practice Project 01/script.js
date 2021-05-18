146059e4078720ade0c117f2c3b685abd6ea5ee5
Arsalan Khan <arsalankhan612621@gmail.com>
2021-05-18 14:30:22 -0700

    Update script.js

--- Practice Project 01/script.js
+++ Practice Project 01/script.js
@@ -26,12 +26,12 @@ function showSuccess(input) {
 }
 
 // Function to check if email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (re.test(input.value.trim())) {
+    if  ( re.test(input.value.trim()) ) {
         showSuccess(input);
     } else {
-        showError(input, `${getFieldId(input)} is not a valid Email`);
+        showError(input, `Please provide a valid email`)
     }
 }
 
@@ -58,6 +58,13 @@ function checkLength(input, min, max) {
     }
 }
 
+//  Function to check if password and confirm password match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, "Passwords don't match")
+    } 
+}
+
 // Function to get the id of input field
 function getFieldId(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -73,4 +80,5 @@ form.addEventListener('submit', function(e) {
     checkLength(username,3,10);
     checkLength(password,6,30);
     checkEmail(email);
-});
+    checkPasswordsMatch(password,password2);
+})
