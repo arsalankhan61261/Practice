c517c2fb4193ef389b4d9fa25af57cc4f50cea61
arsalankhan61261 <82482072+arsalankhan61261@users.noreply.github.com>
2021-06-25 01:18:49 -0700

    JavaScript

--- Practice Project 04/script.js
+++ Practice Project 04/script.js
@@ -1,7 +1,15 @@
+// Get DOM Elements
+const currencyElementOne = document.getElementById('currency-one');
+const amountCurrencyOne = document.getElementById('amount-one');
+const currencyElementTwo = document.getElementById('currency-two');
+const amountCurrencyTwo = document.getElementById('amount-two');
+const rate = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
 function calculate() {
-    fetch('items.json')
-    .then(res => res.json())
-    .then(data => document.body.innerHTML = data[0].text);
+
 };
 
+// Execute calculate function on page load
 calculate();
