4f21eb842640c06561699e10465fc385b8b789ba
arsalankhan61261 <82482072+arsalankhan61261@users.noreply.github.com>
2021-06-27 21:30:32 -0700

    Link.JS

---
+++ Practice Project 04/link/link.js
@@ -0,0 +1,6 @@
+// Get DOM Elements
+const currencyAll = document.getElementById('currency-all');
+const reset = document.getElementById('reset');
+const rate = document.getElementById('rate');
+
+// Fetch exchange rates and update the DOM
