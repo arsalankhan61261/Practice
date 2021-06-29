f5615618b859169606a078f2dc421359c5db740c
arsalankhan61261 <82482072+arsalankhan61261@users.noreply.github.com>
2021-06-29 02:38:07 -0700

    Update link.js

--- Practice Project 04/link/link.js
+++ Practice Project 04/link/link.js
@@ -4,3 +4,24 @@ const reset = document.getElementById('reset');
 const rate = document.getElementById('rate');
 
 // Fetch exchange rates and update the DOM
+function calculate() {
+    // Get the currency code
+    const currencyAllCode = currencyAll.value;
+    
+    // Send Request to ExchangeRate-API for conversion rates for currency all
+    fetch(`https://v6.exchangerate-api.com/v6/f28b90085b7571e121761388/latest/${currencyAllCode}`)
+        .then(res => res.json())
+        .then(data => { console.log(data);
+            // Get the conversion rates from currency all
+            const conversionRates = data.conversion_rates;
+            // Update tha DOM to display the Converion Rate
+            rate.innerText = conversionRates;
+        });
+};
+
+// Event Listeners
+// Event on change a country in currency all
+currencyAll.addEventListener('change', calculate);
+
+// Execute calculate function on page load
+calculate();
