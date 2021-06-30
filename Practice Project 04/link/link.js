8aa6dbf1bdc61f071c3585235e03a7e1023809c6
arsalankhan61261 <82482072+arsalankhan61261@users.noreply.github.com>
2021-06-29 20:07:13 -0700

    Update link.js

--- Practice Project 04/link/link.js
+++ Practice Project 04/link/link.js
@@ -11,7 +11,7 @@ function calculate() {
     // Send Request to ExchangeRate-API for conversion rates for currency all
     fetch(`https://v6.exchangerate-api.com/v6/f28b90085b7571e121761388/latest/${currencyAllCode}`)
         .then(res => res.json())
-        .then(data => { console.log(data);
+        .then(data => { console.log(data.conversion_rates);
             // Get the conversion rates from currency all
             const conversionRates = data.conversion_rates;
             // Update tha DOM to display the Converion Rate
